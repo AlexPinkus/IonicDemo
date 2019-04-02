fb21542758c8463e1e5ae552e0cff644ba623bd4
alexpinkus <alexpinkus@hotmail.com>
2019-04-02 10:14:18 -0600

    Updated quotations page

--- src/app/quotations/quotations.page.ts
+++ src/app/quotations/quotations.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotation } from '../models/Quotation';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from '../components/popover/popover.component';
 @Component({
   selector: 'app-quotations',
   templateUrl: './quotations.page.html',
@@ -7,7 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotationsPage implements OnInit {
 
-  constructor() { }
+  public quotations: Quotation[] = [];
+
+  constructor(public popoverController: PopoverController) {
+    for (let index = 0; index < 5; index++) {
+      this.quotations.push(new Quotation());
+    }
+  }
+
+  async presentPopover(ev: any, quotation: Quotation) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      componentProps: quotation,
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
 
   ngOnInit() {
   }
